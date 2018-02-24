0f543316735f33e5d8e86acad08f33b1798f78da
Hi <Hi@His-MacBook-Pro.local>
2018-02-24 16:46:35 +0100

    first version of the project with initial form for key submission and youtube-video-url submission. Returns a json-object in console. And some info in textfield.

---
+++ googlesetup.js
@@ -0,0 +1,86 @@
+      // Client ID and API key from the Developer Console
+document.querySelector('#keyform').addEventListener('submit', (event) => {
+  event.preventDefault();
+  var CLIENT_ID = document.getElementById('key-input').value;
+  console.log(CLIENT_ID);
+
+  // Array of API discovery doc URLs for APIs used by the quickstart
+  var DISCOVERY_DOCS = ["https://www.googleapis.com/discovery/v1/apis/youtube/v3/rest"];
+
+  // Authorization scopes required by the API. If using multiple scopes,
+  // separated them with spaces.
+  var SCOPES = 'https://www.googleapis.com/auth/youtube.readonly';
+
+  var authorizeButton = document.getElementById('authorize-button');
+  var authorizeLegend = document.getElementById('authorize-legend');
+  var signoutButton = document.getElementById('signout-button');
+  var submitKeyButton = document.getElementById('submitkey-button');
+  var keyInput = document.getElementById('key-input');
+
+      /**
+       *  On load, called to load the auth2 library and API client library.
+       */
+  function handleClientLoad() {
+    gapi.load('client:auth2', initClient);
+  }
+
+
+  /**
+    *  Initializes the API client library and sets up sign-in state
+    *  listeners.
+  */
+  function initClient() {
+    gapi.client.init({
+      discoveryDocs: DISCOVERY_DOCS,
+      clientId: CLIENT_ID,
+      scope: SCOPES
+      }).then(function () {
+          // Listen for sign-in state changes.
+          gapi.auth2.getAuthInstance().isSignedIn.listen(updateSigninStatus);
+
+          // Handle the initial sign-in state.
+          updateSigninStatus(gapi.auth2.getAuthInstance().isSignedIn.get());
+          authorizeButton.onclick = handleAuthClick;
+          signoutButton.onclick = handleSignoutClick;
+        });
+      }
+
+    /**
+      *  Called when the signed in status changes, to update the UI
+       *  appropriately. After a sign-in, the API is called.
+       */
+      function updateSigninStatus(isSignedIn) {
+        if (isSignedIn) {
+          authorizeButton.style.display = 'none';
+          signoutButton.style.display = 'block';
+          submitKeyButton.style.display = 'none';
+          keyInput.style.display = 'none';
+          authorizeLegend.style.display = 'none';
+        } else {
+          authorizeButton.style.display = 'block';
+          authorizeLegend.style.display = 'block';
+          signoutButton.style.display = 'none';
+          submitKeyButton.style.display = 'block';
+          keyInput.style.display = 'block';
+        }
+      }
+
+      /**
+       *  Sign in the user upon button click.
+       */
+      function handleAuthClick(event) {
+        gapi.auth2.getAuthInstance().signIn();
+      }
+
+      /**
+       *  Sign out the user upon button click.
+       */
+      function handleSignoutClick(event) {
+        gapi.auth2.getAuthInstance().signOut();
+      }
+
+      handleClientLoad();
+});
+      
+
+      
---
+++ main.js
@@ -0,0 +1,46 @@
+
+document.querySelector('#urlform').addEventListener('submit', ((event) => {
+  var url = document.getElementById('videourl').value;
+  event.preventDefault();
+  console.log(url);
+  
+  // Function that returns a Youtube ID from a URL of a video
+  function getYoutubeID(url) {
+    var regExp = /^.*(youtu\.be\/|v\/|u\/\w\/|embed\/|watch\?v=|\&v=)([^#\&\?]*).*/;
+    var match = url.match(regExp);
+      if (match && match[2].length == 11) {
+        console.log(match[2]);
+        return match[2];
+      } else {
+        //error
+        console.log('error with url')
+      }
+  }
+
+  function appendResult(video) {
+    // Clear results
+    var p = document.getElementById('resultEl');
+    var videoInfo = 'This video\'s ID is ' + video.id + '. ' +
+                    'Its title is \'' + video.snippet.localized.title + ', ' +
+                    'and it has ' + video.statistics.viewCount + ' views.' + 
+                    'and' + video.statistics.likeCount + ' likes';
+    p.innerHTML = '';
+    p.innerHTML = videoInfo;
+  }
+
+  function getVideo(youtubeID) {
+    // the call to the Youtube API
+    gapi.client.youtube.videos.list({
+      'part': 'snippet, contentDetails, statistics',
+      'id': youtubeID,
+    }).then(function(data) {
+      var result = data.result.items[0];
+      appendResult(result);
+    }); 
+  } // End Get Video
+
+  var youtubeID = getYoutubeID(url);
+  getVideo(youtubeID);
+
+})) // End Submit
+
